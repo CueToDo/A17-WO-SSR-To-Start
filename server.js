608bf67d17b6b5997d1352659a405c7426964d27
Alex Cooper <alex@free.vote>
2024-06-05 21:48:03 +0100

    tweak - app.use?

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-
 const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
-
 const app = express();
 
-app.use('/', express.static(__dirname + '/dist/a17-soc'));
+// Get the port from Heroku environment or default to 8080
+const port = process.env.PORT || 8080;
 
-app.listen(port,()=>{
-    console.log('app started and listening on port:', port)
+// Serve static files from the dist folder
+app.use(express.static(__dirname + '/dist/a17-soc'));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+// Handles any request that doesn't match a static file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
