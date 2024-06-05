22a8416c89c34b78a4bdd714c7be7263c6727e46
Alex Cooper <alex@free.vote>
2024-06-05 20:42:31 +0100

    require https low confidence

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/a17-soc'));
+// app.use(express.static(__dirname + '/dist/a17-soc'));
+
+app.use(requireHTTPS());
+
 
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname + '/dist/a17-soc/index.html'));
